4f951a3ae1d4d147fcd9262b505b0fbb768b8fe4
verma-rishabh <rishabh754@gmail.com>
2023-07-18 21:55:44 +0200

    reward modified

--- air_hockey_agent/agent_builder_tqc.py
+++ air_hockey_agent/agent_builder_tqc.py
@@ -14,5 +14,5 @@ def build_agent(env_info, **kwargs):
     """
 
     agent = TQC_agent(env_info,agent_id=1)
-    agent.load("home/captain/SS23/DeepLearning Lab/Project/air_hockey_qualifying/hit/models/tqc-hit-mushroomrl_7dof-hit_0")
+    agent.load("../hit/atacom/models/tqc-hit-mushroomrl_7dof-hit_0")
     return agent
